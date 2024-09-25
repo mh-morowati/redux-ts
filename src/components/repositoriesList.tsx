1a3ac4409a9874d286be823ba47171f58305d0bf
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2024-09-25 19:48:15 +0330

    jhg

--- src/components/repositoriesList.tsx
+++ src/components/repositoriesList.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { useAction } from "../hooks/useAction";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () =>{
     const [term,setTerm] = useState('');
     const {searchRepositories} = useAction();
+    const {data,error,loading} = useTypedSelector((state) => state.repositories)
 
     const onsubmit = (event: React.FormEvent<HTMLFormElement>) =>{
         event.preventDefault();
@@ -17,6 +19,9 @@ return <div>
         <input type="text" value={term} onChange={e => setTerm(e.target.value)}/>
         <button>Search</button>
     </form>
+    {error && <h3>{error}</h3>}
+    {loading && <h3>loading...</h3>}
+    {!error && !loading && data.map((name) => <div key={name}>{name}</div>) }
 </div>
 }
 
